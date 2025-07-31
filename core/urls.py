92361350d48e15211eaf96c9dfb49af5d548e962
Daniel Erick <DanielErick.estudante@gmail.com>
2025-07-31 17:50:53 -0300

    testando deploy

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
 from django.contrib.auth import views
 
 
@@ -8,5 +7,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.LoginView.as_view(), name='login'),
     path("logout/", views.LogoutView.as_view(), name='logout'),
-    path('', lambda request: HttpResponse("<h1>Projeto Minimo Funcionou</h1>"))
 ]
