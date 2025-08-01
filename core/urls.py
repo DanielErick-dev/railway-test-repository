f1929b163fecb20729b49f4413d3c8c047bb10d1
Daniel Erick <DanielErick.estudante@gmail.com>
2025-08-01 13:52:04 -0300

    refactor(urls): updated urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views
-from .views import criar_superusuario_secreto
 
 
 urlpatterns = [
@@ -9,5 +8,4 @@ urlpatterns = [
     path("", views.LoginView.as_view(), name='login'),
     path("logout/", views.LogoutView.as_view(), name='logout'),
     path("students/", include('students.urls')),
-    path('super_usuario/', criar_superusuario_secreto, name='super_usuario')
 ]
