851576204e3ace7d42427302b3194c1302eadb5d
Daniel Erick <DanielErick.estudante@gmail.com>
2025-08-03 22:57:00 -0300

    refactor(settings): organizes settings configuration

--- core/settings.py
+++ core/settings.py
@@ -3,12 +3,25 @@ import os
 from decouple import config
 import dj_database_url
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-local-dev-key-!@#$%^&*()')
 
+
 DEBUG = config('DEBUG', default=True, cast=bool)
+
 ENVIRONMENT = config('ENVIRONMENT', default='development')
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*']
+
+
+IS_BUILD_PHASE = config('IS_BUILD_PHASE', default=False, cast=bool)
+
+if not IS_BUILD_PHASE:
+    CALLMEBOT_URL = config('CALLMEBOT_URL')
+    CALLMEBOT_API_KEY = config('CALLMEBOT_API_KEY')
+    CALLMEBOT_PHONE_NUMBER = config('CALLMEBOT_PHONE_NUMBER')
 
 
 INSTALLED_APPS = [
@@ -34,12 +47,6 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-LOGIN_URL = '/'
-
-LOGIN_REDIRECT_URL = '/students/list/'
-
-LOGOUT_REDIRECT_URL = '/'
-
 ROOT_URLCONF = "core.urls"
 
 TEMPLATES = [
@@ -49,6 +56,7 @@ TEMPLATES = [
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
+                "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
@@ -59,70 +67,48 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "core.wsgi.application"
 
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": config('POSTGRES_DB', default='db_name'),
-        "USER": config('POSTGRES_USER', default='db_user'),
-        "PASSWORD": config('POSTGRES_PASSWORD', default='db_pass'),
-        "HOST": config('POSTGRES_HOST', default='localhost'),
-        "PORT": config('POSTGRES_PORT', default=5432, cast=int),
+        "NAME": config('POSTGRES_DB'),
+        "USER": config('POSTGRES_USER'),
+        "PASSWORD": config('POSTGRES_PASSWORD'),
+        "HOST": config('POSTGRES_HOST'),
+        "PORT": config('POSTGRES_PORT', cast=int),
     }
 }
 
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
+    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
-
 LANGUAGE_CODE = "pt-BR"
-
 TIME_ZONE = "America/Sao_Paulo"
-
 USE_I18N = True
-
 USE_TZ = True
 
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+LOGIN_URL = '/'
+LOGIN_REDIRECT_URL = '/students/list/'
+LOGOUT_REDIRECT_URL = '/'
 
 if ENVIRONMENT == 'production':
+    DEBUG = False
     ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
-    CSRF_TRUSTED_ORIGINS = config(
-        'CSRF_TRUSTED_ORIGINS',
-        cast=lambda v: [s.strip() for s in v.split(',')]
-    )
-
     DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+    CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', cast=lambda v: [s.strip() for s in v.split(',')])
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-
-IS_BUILD_PHASE = config('IS_BUILD_PHASE', default=False, cast=bool)
-
-if not IS_BUILD_PHASE:
-    CALLMEBOT_URL = config('CALLMEBOT_URL')
-    CALLMEBOT_API_KEY = config('CALLMEBOT_API_KEY')
-    CALLMEBOT_PHONE_NUMBER = config('CALLMEBOT_PHONE_NUMBER')
-
