0af652a468e1a5fd75c21a9600d6c4e314ba47f0
Daniel Erick <DanielErick.estudante@gmail.com>
2025-08-04 21:50:55 -0300

    fix(cron): fix python path

--- run_tasks.sh
+++ run_tasks.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+
 cd /app
 echo "--- [CRON] Iniciando tarefas agendadas..."
-python manage.py update_user_status
-python manage.py send_message
+/usr/local/bin/python manage.py update_user_status
+/usr/local/bin/python manage.py send_message
 echo "--- [CRON] Tarefas agendadas finalizadas."
